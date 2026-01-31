a987db7cf1a3063236ddd64851d773b76df26d27
Bear112006 <jonathanslattery112006@gmail.com>
2026-01-30 22:00:38 -0500

    Create helperFunctions.php

---
+++ ContactsAPI/helperFunctions.php
@@ -0,0 +1,24 @@
+<?php
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"FirstName":"' . $firstName . '","LastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
