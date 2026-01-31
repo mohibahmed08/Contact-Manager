2007a7b3f0cf7bf3da5a8c03893fe03e21aae0fc
Bear112006 <jonathanslattery112006@gmail.com>
2026-01-30 22:01:32 -0500

    Remove shared helper functions from signup.php

--- ContactsAPI/signup.php
+++ ContactsAPI/signup.php
@@ -1,5 +1,7 @@
 
 <?php
+    require_once "helperFunctions.php";
+    
     function handleSignup($inData) {
         
         $id = 0;
@@ -37,28 +39,5 @@
             return;
         }
     }
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"FirstName":"' . $firstName . '","LastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
 
 ?>
