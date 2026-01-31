5eb89be5fde57dc44654f0f35d64de509bb04d8f
Bear112006 <jonathanslattery112006@gmail.com>
2026-01-30 21:58:31 -0500

    Turn into login.php into function for master API usage

--- ContactsAPI/login.php
+++ ContactsAPI/login.php
@@ -1,41 +1,41 @@
 
 <?php
+    function handleLogin($inData) {
+        
+        $id = 0;
+        $firstName = "";
+        $lastName = "";
 
-	$inData = getRequestInfo();
-	
-	$id = 0;
-	$firstName = "";
-	$lastName = "";
+        $conn = new mysqli("localhost", "PLACEHOLDER_USERNAME", "PLACEHOLDER_PASSWORD", "ContactManager"); 	
+        if( $conn->connect_error )
+        {
+            returnWithError( $conn->connect_error );
+        }
+        else
+        {
+            $stmt = $conn->prepare("SELECT ID, FirstName, LastName, Password FROM Users WHERE Login=?");
+            $stmt->bind_param("s", $inData["Login"]);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-	$conn = new mysqli("localhost", "PLACEHOLDER_USERNAME", "PLACEHOLDER_PASSWORD", "ContactManager"); 	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("SELECT ID, FirstName, LastName, Password FROM Users WHERE Login=?");
-		$stmt->bind_param("s", $inData["Login"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
-
-		if( $row = $result->fetch_assoc()  )
-		{
-            if (password_verify($inData["Password"], $row['Password'])) {
-			    returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
-            } else {
+            if( $row = $result->fetch_assoc()  )
+            {
+                if (password_verify($inData["Password"], $row['Password'])) {
+                    returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
+                } else {
+                    returnWithError("Invalid Login");
+                }
+            }
+            else
+            {
                 returnWithError("Invalid Login");
             }
-		}
-		else
-		{
-			returnWithError("Invalid Login");
-		}
 
-		$stmt->close();
-		$conn->close();
-        exit();
-	}
+            $stmt->close();
+            $conn->close();
+            return;
+        }
+    }
 	
 	function getRequestInfo()
 	{
