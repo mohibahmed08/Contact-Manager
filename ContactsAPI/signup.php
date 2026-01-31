6f7abe82bb97da256b70d0491f51e9674dc5bcb2
Bear112006 <jonathanslattery112006@gmail.com>
2026-01-30 21:54:34 -0500

    Change into a callable function for later master API

--- ContactsAPI/signup.php
+++ ContactsAPI/signup.php
@@ -1,42 +1,42 @@
 
 <?php
+    function handleSignup($inData) {
+        
+        $id = 0;
+        $firstName = "";
+        $lastName = "";
 
-	$inData = getRequestInfo();
-	
-	$id = 0;
-	$firstName = "";
-	$lastName = "";
+        $conn = new mysqli("localhost", "PLACEHOLDER_USERNAME", "PLACEHOLDER_PASSWORD", "ContactManager"); 	
+        if( $conn->connect_error )
+        {
+            returnWithError( $conn->connect_error );
+        }
+        else
+        {
+            // Insert new account into database
+            $stmtInsert = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+            $hashedPassword = password_hash($inData["password"], PASSWORD_DEFAULT); // Hashes password for better security incase database is hacked
+            $stmtInsert->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $inData["login"], $hashedPassword);
 
-	$conn = new mysqli("localhost", "PLACEHOLDER_USERNAME", "PLACEHOLDER_PASSWORD", "ContactManager"); 	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-        // Insert new account into database
-        $stmtInsert = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
-        $hashedPassword = password_hash($inData["password"], PASSWORD_DEFAULT); // Hashes password for better security incase database is hacked
-		$stmtInsert->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $inData["login"], $hashedPassword);
+            if (!$stmtInsert->execute()) {
+                if ($conn->errno == 1062) 
+                    returnWithError("Account already exists");
+                else 
+                    returnWithError("Failed to create account: " . $conn->error);
+                
+                $stmtInsert->close();
+                $conn->close();
+                exit();
+            }
 
-        if (!$stmtInsert->execute()) {
-            if ($conn->errno == 1062) 
-                returnWithError("Account already exists");
-            else 
-                returnWithError("Failed to create account: " . $conn->error);
-            
+            returnWithInfo( $inData['FirstName'], $inData['LastName'], $conn->insert_id );
+
             $stmtInsert->close();
             $conn->close();
+
             exit();
         }
-
-        returnWithInfo( $inData['FirstName'], $inData['LastName'], $conn->insert_id );
-
-		$stmtInsert->close();
-		$conn->close();
-
-        exit();
-	}
+    }
 	
 	function getRequestInfo()
 	{
@@ -60,5 +60,5 @@
 		$retValue = '{"id":' . $id . ',"FirstName":"' . $firstName . '","LastName":"' . $lastName . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 ?>
