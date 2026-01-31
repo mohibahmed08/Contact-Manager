aa9bb8845329951c7ea9cc0586bce3fb62b60585
Bear112006 <jonathanslattery112006@gmail.com>
2026-01-30 22:02:07 -0500

    Remove shared helper functions from login.php

--- ContactsAPI/login.php
+++ ContactsAPI/login.php
@@ -1,5 +1,7 @@
 
 <?php
+    require_once "helperFunctions.php";
+    
     function handleLogin($inData) {
         
         $id = 0;
@@ -35,29 +37,5 @@
             $conn->close();
             return;
         }
-    }
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"FirstName":"' . $firstName . '","LastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+    }	
 ?>
