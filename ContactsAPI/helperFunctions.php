166f5280d2bdcba67137aa59070b7f94be9459c6
Bear112006 <jonathanslattery112006@gmail.com>
2026-01-30 23:10:49 -0500

    Update helperFunctions.php to cover Contact returns

--- ContactsAPI/helperFunctions.php
+++ ContactsAPI/helperFunctions.php
@@ -21,4 +21,31 @@
 		$retValue = '{"id":' . $id . ',"FirstName":"' . $firstName . '","LastName":"' . $lastName . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
+
+    function returnContactWithInfo ( $rows ) 
+    {
+        // Convert to Json
+        $retValue = '{ "results": [';
+        $rowNum = 0;
+        foreach ($rows as $row) {
+            if ($rowNum > 0) {
+                $retValue .= ",";
+            }
+
+            $rowNum++;
+
+            $retValue .= '{"id":' . $row["ID"] . ',"FirstName":"' . $row["FirstName"] . '","LastName":"' . $row["LastName"] . '","Phone":"' . $row["Phone"] . '","Email":"' . $row["Email"] . '"}';
+        }
+
+        $retValue .= '], "error":"" }'; 
+
+        // Return
+        sendResultInfoAsJson( $retValue );
+    }
+
+    function returnContactWithError ( $err ) 
+    {
+        $retValue = '{"results": [], "error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+    }
 ?>
