5ff3c2f34f7a6d3dad80ea87656191fce5ff0525
Curel-X <jasoncomras@aol.com>
2026-02-01 17:14:04 -0500

    Start HTML & CSS Template

---
+++ ContactEdit.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Contact Edit page loaded");
+
+    // Example: grab elements
+    const initials = document.getElementById("edit-contact-initials");
+});
