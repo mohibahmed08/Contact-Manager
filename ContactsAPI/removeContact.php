2185b099cda77d114fd4b41d59820b3d01c707ba
Bear112006 <jonathanslattery112006@gmail.com>
2026-02-04 21:07:55 -0500

    Make removeContact.php an endpoint

--- ContactsAPI/removeContact.php
+++ ContactsAPI/removeContact.php
@@ -1,41 +1,42 @@
 <?php
     require_once "helperFunctions.php";
 
-    function handleRemoveContact($inData) {
-        if (!isset($inData["UserID"], $inData["ID"])) {
-            returnWithError("Missing required fields");
-            return;
-        }
-        $id = 0;
+    $inData = json_decode(file_get_contents("php://input"), true);
 
-        $conn = new mysqli("localhost", "API", "admin1234", "ContactManager"); 	
-        if( $conn->connect_error )
-        {
-            returnWithError( $conn->connect_error );
-        }
-        else
-        {
-            $stmt = $conn->prepare("DELETE FROM Contacts WHERE UserID = ? AND ID = ?;");
-            $stmt->bind_param("ii", $inData["UserID"], $inData["ID"]);
+    if (!isset($inData["UserID"], $inData["ID"])) {
+        returnWithError("Missing required fields");
+        return;
+    }
+    $id = 0;
 
-            if (!$stmt->execute()) {
-                returnWithError("Deletion failed: " . $stmt->error);
-                $stmt->close();
-                $conn->close();
-                return;
-            }
+    $conn = new mysqli("localhost", "API", "admin1234", "ContactManager"); 	
+    if( $conn->connect_error )
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        $stmt = $conn->prepare("DELETE FROM Contacts WHERE UserID = ? AND ID = ?;");
+        $stmt->bind_param("ii", $inData["UserID"], $inData["ID"]);
 
-            sendResultInfoAsJson(json_encode([
-                "success" => true,
-                "affectedRows" => $stmt->affected_rows,
-                "error" => ""
-            ]));
-
+        if (!$stmt->execute()) {
+            returnWithError("Deletion failed: " . $stmt->error);
             $stmt->close();
             $conn->close();
-
             return;
         }
-    }	
+
+        sendResultInfoAsJson(json_encode([
+            "success" => true,
+            "affectedRows" => $stmt->affected_rows,
+            "error" => ""
+        ]));
+
+        $stmt->close();
+        $conn->close();
+
+        return;
+    }
+
     
 ?>
