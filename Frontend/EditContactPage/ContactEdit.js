414387d7d81942f9b1f89bcd5df65f1852569032
Curel-X <jasoncomras@aol.com>
2026-02-06 12:24:03 -0500

    Start back button w/ link

--- Frontend/EditContactPage/ContactEdit.js
+++ Frontend/EditContactPage/ContactEdit.js
@@ -172,7 +172,6 @@ window.addEventListener("beforeunload", (e) => {
     localStorage.removeItem("contactInfo");
 });
 
-
 //IF BACK BUTTON IS CLICKED
 document.getElementById("back-button").addEventListener("click", function () {
     //SWITCH BACK TO THE CONTACT PAGE
